b497fd3178657446e4583dc5d8fa9f38c79e012f
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2023-07-11 09:42:22 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pyairstage
 
 Python Library for interacting with Fujitsu Airstage Airconditioners API.
+
+Inspired from https://github.com/thierryvt/pyfujitsu
--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pyairstage",
-    version="1.0.0",
     author="Daniel Rufus Kaldheim",
     author_email="daniel@kaldheim.org",
     description="Python library to control Fujitsu Airstage Airconditioners",
-    long_description="Python library to control Fujitsu Airstage Airconditioners",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/danielkaldheim/pyairstage",
     license="MIT License",
     packages=["pyairstage"],
-    install_requires=["requests", "certifi", "chardet", "idna", "urllib3"],
+    install_requires=["aiohttp", "certifi", "chardet", "idna", "urllib3"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
